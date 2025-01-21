d514873208dd94340fbbc55b305ea8107b5d7008
Habis Muhammed <70653261+BrownCoatJustice@users.noreply.github.com>
2025-01-21 12:35:48 +0530

    Init commit once again
    messed up bad

---
+++ src/main/java/org/duckdns/habism/App.java
@@ -0,0 +1,12 @@
+package org.duckdns.habism;
+
+/**
+ * @author Habis Muhammed
+ */
+public final class App {
+    
+    public static void main(String[] args) {
+        System.out.println("Hello World! This is 'grove'!");
+        
+    }
+}
