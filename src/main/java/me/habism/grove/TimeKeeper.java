3e9ffd59b1f20efbf01fb302ad7c6a32a6881f05
Habis Muhammed <brownforsocialmedia@gmail.com>
2025-05-18 19:08:16 +0530

    refactor(app): move config logic to App.java and update save path

--- src/main/java/me/habism/grove/TimeKeeper.java
+++ src/main/java/me/habism/grove/TimeKeeper.java
@@ -1,11 +1,20 @@
 package me.habism.grove;
 
-import java.util.Scanner;
 import java.util.Timer;
 import java.util.TimerTask;
+import java.util.logging.Logger;
+import java.util.logging.Level;
 
+import static me.habism.grove.App.sc;
+
+/**
+ *
+ * @author habism
+ * Handles anything that primarily uses a clock.
+ */
 public class TimeKeeper {
 
+    private static final Logger logger = Logger.getLogger(TimeKeeper.class.getName());
     private static volatile boolean isTerminated = false;
 
     private int seshMins = 25;
@@ -14,10 +23,10 @@ public class TimeKeeper {
 
     private boolean isPomodoro = false;
     private int sessionCount = 0;
-    private int remainingTime;
-    private Timer timer;
+    private int remainingTime; // in seconds
+    private int lastSavedMinute = -1;
 
-    private static Scanner eofScanner;
+    private Timer timer;
 
     public void setPomodoroMode(boolean isPomodoro) {
         this.isPomodoro = isPomodoro;
@@ -28,7 +37,7 @@ public class TimeKeeper {
             throw new IllegalArgumentException("Session duration too long.");
         }
         this.seshMins = seshMins;
-        System.out.println("Session time set to " + seshMins + " minutes.");
+        logger.log(Level.INFO, "Session time set to {0} minutes.", seshMins);
     }
 
     public void setShortBreak(int mins) {
@@ -36,11 +45,11 @@ public class TimeKeeper {
             throw new IllegalArgumentException("Invalid short break duration.");
         }
         this.sBreakMins = mins;
-        System.out.println("Short break time set to " + mins + " minutes.");
+        logger.log(Level.INFO, "Short break set to {0} minutes.", mins);
     }
 
-    public void setLongBreak(int mins) throws IllegalArgumentException {
-        if (mins == 360) {
+    public void setLongBreak(int mins) {
+        if (mins == 360) { // Easter Egg, shhh!
             try {
                 System.out.println("From the screen to the ring, to the pen, to the king");
                 Thread.sleep(3000);
@@ -50,14 +59,21 @@ public class TimeKeeper {
                 Thread.sleep(2000);
             } catch (InterruptedException e) {
                 Thread.currentThread().interrupt();
+                logger.warning("Interrupted during easter egg.");
             }
         }
 
         if (mins > 999) {
             throw new IllegalArgumentException("Invalid long break duration.");
         }
+
         this.lBreakMins = mins;
-        System.out.println("Long break time set to " + mins + " minutes.");
+        logger.log(Level.INFO, "Long break set to {0} minutes.", mins);
+    }
+
+    private short getTickSpeed() {
+        // TODO: Tell me why I shouldn't continue with the 'short' data type.
+        return (short) (App.inDebugMode ? 1 : 1000);
     }
 
     public void startPomodoro() {
@@ -72,11 +88,13 @@ public class TimeKeeper {
 
     private void startSession() {
         if (isTerminated) {
-            System.out.println("[Session aborted] Termination signal received before session start.");
+            logger.warning("Session aborted: Termination signal received before start.");
             return;
         }
+
         remainingTime = seshMins * 60;
-        System.out.println("Session started for " + seshMins + " minutes. You can always stop by pressing Ctrl+D");
+        System.out.println("Session started for " + seshMins + " minutes.");
+        System.out.println("You can stop anytime using Ctrl+D (Linux/macOS) or Ctrl+Z (Windows).");
 
         timer = new Timer();
         timer.scheduleAtFixedRate(new TimerTask() {
@@ -89,34 +107,18 @@ public class TimeKeeper {
                         System.out.println((remainingTime / 60) + " minutes remaining...");
                     }
                     remainingTime--;
+                    maybeAutosave();
                 }
             }
-        }, 0, 1000);
-    }
-
-    private void onSessionComplete() {
-        System.out.println("Session completed!");
-
-        if (isPomodoro) {
-            sessionCount++;
-            if (sessionCount % 4 == 0) {
-                System.out.println("Taking a long break of " + lBreakMins + " minutes.");
-                startBreak(lBreakMins);
-            } else {
-                System.out.println("Taking a short break of " + sBreakMins + " minutes.");
-                startBreak(sBreakMins);
-            }
-        } else {
-            System.out.println("[Single] Session completed. Good job!");
-            stopSession();
-        }
+        }, 0, getTickSpeed());
     }
 
     private void startBreak(int breakMinutes) {
         if (isTerminated) {
-            System.out.println("[Break aborted] Termination signal received before break start.");
+            logger.warning("Break aborted: Termination signal received before break start.");
             return;
         }
+
         remainingTime = breakMinutes * 60;
         System.out.println("\nBreak started for " + breakMinutes + " minutes.");
 
@@ -134,26 +136,52 @@ public class TimeKeeper {
                     remainingTime--;
                 }
             }
-        }, 0, 1000);
-
+        }, 0, getTickSpeed());
     }
 
-    // see APP_LOGIC.md#Stop Pomodor Logic or smth
+    private void onSessionComplete() {
+        if (isPomodoro) {
+            sessionCount++;
+            int breakDuration = (sessionCount % 4 == 0) ? lBreakMins : sBreakMins;
+            System.out.println("Taking a " + ((sessionCount % 4 == 0) ? "long" : "short") + " break of " + breakDuration + " minutes.");
+            startBreak(breakDuration);
+        } else {
+            System.out.println("[Single] Session completed. Good job!");
+            //sc.close();
+            stopSession();
+        }
+    }
+
     public void stopSession() {
         isTerminated = true;
 
         if (timer != null) {
-            this.timer.cancel();
+            timer.cancel();
         }
 
-        int currentFocusedTime = (seshMins * 60) - remainingTime;
-        int totalSecondsFocused = (sessionCount * seshMins * 60) + currentFocusedTime;
+        int focusedSeconds = (seshMins * 60) - remainingTime;
+        int totalSecondsFocused = (sessionCount * seshMins * 60) + focusedSeconds;
         int minutesFocused = totalSecondsFocused / 60;
 
-        System.out.println("\nSession manually stopped!");
+        // saves dont work in debug mode due to tick speed modding. instead here's a debug mode backup.
+        if (App.inDebugMode && minutesFocused > 0) {
+            App.stats.totalTimeFocused += minutesFocused;
+        }
+
+        System.out.println("\nSession stopped.");
         System.out.println("Total time focused: " + minutesFocused + " minutes");
 
-        // Optional feedback messages
+        // Streak will only update if time focused is at least 5min
+        if (minutesFocused >= 5) {
+            App.updateFocusStreak();
+            System.out.println("Current focus streak: " + App.stats.focusStreak + " days.");
+        }
+        try {
+            StatMan.saveStats(App.stats);
+        } catch (Exception e) {
+            logger.log(Level.WARNING, "An unknown error has occurred. Your stats failed to save. Check .../grove/data/stats.json");
+        }
+        
         if (minutesFocused >= 50) {
             System.out.println("Beast mode. Big focus.");
         } else if (minutesFocused >= 25) {
@@ -161,14 +189,30 @@ public class TimeKeeper {
         } else {
             System.out.println("Every bit counts. Tomorrow’s another chance.");
         }
+        
+        System.exit(0);
+        
+        /*sc.nextLine();
+        System.exit(0);*/
+    }
+
+    private void maybeAutosave() {
+        int minutesFocused = seshMins - (remainingTime / 60);
+
+        if (minutesFocused != lastSavedMinute && remainingTime % 60 == 0 && !App.inDebugMode) {
+            App.stats.totalTimeFocused++;
+            StatMan.saveStats(App.stats);
+            lastSavedMinute = minutesFocused;
+            logger.log(Level.FINE, "Autosaved at minute: {0}", minutesFocused);
+        }
     }
 
     public void watchForEOF() {
-        eofScanner = new Scanner(System.in);
-        while (eofScanner.hasNextLine()) {
-            eofScanner.nextLine();
+        while (sc.hasNextLine()) {
+            sc.nextLine();
         }
-        System.out.println("EOF received. Stopping...");
+        logger.info("EOF received. Stopping session...");
         stopSession();
     }
+
 }
